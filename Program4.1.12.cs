ac3af483367fb3bd64382621d21200fa89ecf4a4
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-20 22:47:59 +0300

    Add files via upload

---
+++ Program4.1.12.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var a = 6;
+			var b = 7;
+			
+
+			if (a == b)
+			{
+				Console.WriteLine("Условие истинно");
+			}
+			else
+			{
+				Console.WriteLine("Условие ложно");
+			}
+			if (b < 10)
+			{
+
+				Console.WriteLine("Значение b={0} меньше 10", b);
+			}
+			else
+			{
+				Console.WriteLine("Значение b={0} больше 10", b);
+			
+			
+			}
+			Console.ReadKey();
+
+
+		}		
+    }
+}

8a01226fb19da835518ca8aada01de163ef2976a
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-20 21:43:41 +0300

    Add files via upload

---
+++ Program4.1.5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 2;
+            double X = 3;
+            double Y = 4;
+            bool C = (A < B) | (X > Y);
+            Console.WriteLine(C);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}

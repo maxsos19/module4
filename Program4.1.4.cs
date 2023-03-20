057f97a353bd0a3595af7c7e47af72efa2eb5a98
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-20 21:25:04 +0300

    Add files via upload

---
+++ Program4.1.4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "me";
+            string B = "my";
+            bool C = A != B;
+            Console.WriteLine(C);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}

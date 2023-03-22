815651eb6111a2082ef29b1c7b4d9d818668085a
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-22 19:19:48 +0300

    Добавил конструкцию switch

---
+++ Program4.1.17.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+        {
+            Console.WriteLine("Введите ваш любимый цвет (красный, зеленый, голубой)");
+            switch(Console.ReadLine())
+            { 
+                case "красный":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Ваш цвет красный");
+                break;
+
+                case "зеленый":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Ваш цвет зеленый");
+                    break;
+                case "голубой":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Ваш цвет бирюзовый");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Ваш цвет желтый");
+                    break;
+
+
+            }
+            Console.ReadKey();
+            
+       }
+    }
+}

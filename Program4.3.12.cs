1e12bb9d2faee3793d4e7a3b051e66d105c13df5
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-27 02:25:43 +0300

    Add files via upload

---
+++ Program4.3.12.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+

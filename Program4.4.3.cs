b3a0ddf6a07041aedcb1aed0f906d164a67ce6d9
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-28 16:53:37 +0300

    Add files via upload

---
+++ Program4.4.3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var (name, age) = ("Максим", 19);
+            
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
+            Console.WriteLine("Введите ваше имя: ");
+             name = Console.ReadLine();
+            Console.WriteLine("Введите возраст с цифрами: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
+
+            Console.ReadKey();
+        }
+    }
+}
